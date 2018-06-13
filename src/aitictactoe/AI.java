0c6367cd4c99bf47f424bb9c5c8ec1cca5333b00
Raj Rahane <rajrahane10399@gmail.com>
2018-06-13 10:42:52 +0530

    create Board,UI and AI classes

---
+++ src/aitictactoe/AI.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aitictactoe;
+
+import java.util.Random;
+
+/**
+ *
+ * @Raj Rahane
+ * 12 June 2018
+ */
+public class AI {
+    public int findBestMove(Board Board){
+        int []board=Board.getBoard();
+        int bestValue=-1000;
+        int bestMove=-1;
+        int moveValue;
+        for(int i=0;i<Board.TOTALMOVES;i++){
+            if(Board.isBlank(i)){
+                board[i]=Board.getComputer();
+                moveValue=minimax(board,0,false,Board);
+                board[i]=Board.BLANK;
+                
+                if(moveValue>bestValue){                    //***if multiple moves available, randomise
+                    bestValue=moveValue;
+                    bestMove=i;
+                }
+            }
+        }
+        return bestMove;
+    }
+    private int minimax(int[] board,int depth,boolean isMaximiserTurn,Board Board){
+        int score=Board.evaluateBoard(board);
+        if(score>0){                            //computer wins
+            return score-depth;
+        }
+        if(score<0){
+            return score+depth;
+        }        
+        
+        if(!Board.isMovesLeft(board)){
+            return 0;
+        }
+        int best;
+        if(isMaximiserTurn){
+            best=-1000;
+            for(int i=0;i<Board.TOTALMOVES;i++){
+                if(Board.isBlank(i)){
+                    board[i]=Board.getComputer();
+                    best=Math.max(best,minimax(board,depth+1,!isMaximiserTurn,Board));
+                    board[i]=Board.BLANK;
+                }
+            }
+            return best-depth;
+        }
+        else{
+            best=1000;
+            int worstForUser=-1000;                                               //user may or may not play ideally,hence the variable to maximise win chances
+            for(int i=0;i<Board.TOTALMOVES;i++){
+                if(Board.isBlank(i)){
+                    board[i]=Board.getPlayer();
+                    best=Math.min(best,minimax(board,depth+1,!isMaximiserTurn,Board));
+                    worstForUser=Math.max(worstForUser,minimax(board,depth+1,!isMaximiserTurn,Board));
+                    board[i]=Board.BLANK;
+                }
+            }
+            if(best>0){
+                return worstForUser-depth;
+            }
+        }        
+        return best+depth;
+    }
+    public int getRandomCorner() {                             //selects random corner-1,3,7 or 9
+        int corners[]={1,3,7,9};                                // 1 based indexing
+        int rnd=new Random().nextInt(corners.length);
+        return corners[rnd];
+    }
+}
---
+++ src/aitictactoe/Board.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aitictactoe;
+
+/**
+ *
+ * @Raj Rahane
+ * 12 June 2018
+ */
+public class Board {
+
+    private int board[];//board of nine elements
+    static final int computerWins=25;
+    static final int playerWins=-25;
+    static final int TOTALMOVES=9;    
+    static final int BLANK = 2;
+    static final int X = 3;
+    static final int O = 5;
+    private boolean isCompX=false;
+    private int player=X;
+    private int computer=O;
+    private int turn;
+
+    public Board(){
+        board = new int[TOTALMOVES];
+        for (int i = 0; i < TOTALMOVES; i++) {
+            board[i] = BLANK;//BLANK
+        }
+        turn = 1;
+    }
+    public void printBoard() {
+        for (int i = 0; i < TOTALMOVES; i++) {
+            if (i % 3 == 0) {
+                System.out.println();
+            }
+            if (board[i] == BLANK) {
+                System.out.print(" ?");
+            } else if (board[i] == X) {
+                System.out.print(" X");
+            } else {
+                System.out.print(" O");
+            }
+        }
+        System.out.println();
+    }
+    private int decideWinner(int ch){
+        if(isCompX){
+            if(ch==X)
+                return computerWins;
+            else if(ch==O)
+                return playerWins;
+        }
+        else{
+            if(ch==O)
+                return computerWins;
+            else if(ch==X)
+                return playerWins;
+        }
+        return -1;
+    } 
+    public int evaluateBoard(int []board){
+        int result;
+        if(board[0]==board[4]&& board[4]==board[8]){          //diag1
+            result=decideWinner(board[0]);
+            if(result!=-1)
+                return result;
+        }
+        if(board[2]==board[4]&& board[4]==board[6]){        //diag2
+            result=decideWinner(board[2]);
+            if(result!=-1)
+                return result;
+        }
+        if(board[0]==board[1]&& board[1]==board[2]){         //123
+            result=decideWinner(board[0]);
+            if(result!=-1)
+                return result;
+        }
+        if(board[3]==board[4]&& board[4]==board[5]){        //456
+            result=decideWinner(board[3]);
+            if(result!=-1)
+                return result;
+        }
+        if(board[6]==board[7]&& board[7]==board[8]){        //789
+            result=decideWinner(board[6]);
+            if(result!=-1)
+                return result;
+        }
+        if(board[0]==board[3]&& board[3]==board[6]){         //147
+            result=decideWinner(board[0]);
+            if(result!=-1)
+                return result;
+        }
+        if(board[1]==board[4]&& board[4]==board[7]){         //258
+            result=decideWinner(board[1]);
+            if(result!=-1)
+                return result;
+        }
+        if(board[2]==board[5]&& board[5]==board[8]){          //369
+            result=decideWinner(board[2]);
+            if(result!=-1)
+                return result;
+        }
+        return 0;                                             //draw
+    }
+    public boolean isBlank(int i) {                        // 0 based Indexing
+        return board[i] == BLANK ? true : false;       
+    }
+    public boolean isMovesLeft(int board[]){
+        for (int i = 0; i<TOTALMOVES; i++)        
+            if (board[i]==BLANK)
+                return true;
+        return false;
+    }
+    public void setPlayers() {
+        isCompX = true;//cpu is X
+        player=O;
+        computer=X;
+    }
+    
+    public int getComputer(){
+        return computer;
+    }
+    public int getPlayer(){
+        return player;
+    }
+    public boolean isCompX(){
+        return isCompX;
+    }
+    public int[] getBoard(){
+        return board;
+    }
+    public void setBoardIndex(int index,int player){
+        board[index]=player;
+    }
+}
