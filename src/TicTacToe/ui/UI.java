58512cbb2d5b387550906c2d218d5bc835c4953d
Raj Rahane <rajrahane10399@gmail.com>
2018-07-19 13:34:23 +0530

    create classes-GameConstants & Player, code refactoring for impl to decide turn and type of input for player & comp

--- src/TicTacToe/ui/UI.java
+++ src/TicTacToe/ui/UI.java
@@ -3,9 +3,13 @@
  * and open the template in the editor.
  */
 
-package TicTacToe;
+package tictactoe.ui;
 
+import tictactoe.ai.AI;
+import tictactoe.model.Board;
 import java.util.Scanner;
+import tictactoe.model.GameConstants;
+import tictactoe.model.Player;
 
 /**
  *
@@ -17,17 +21,15 @@ public class UI {
         Board board = new Board();
         UI ui=new UI();
         AI ai=new AI();
+        Player player=new Player(),computer=new Player();
+        
         System.out.println("Enter type:1-X? or 2-O");
-        int option = sc.nextInt();
-        if (option == 1) {
-            board.setPlayerToX(true);
-        }
-        System.out.println("Play First?:Yes-1,No-0");
-        option=sc.nextInt();
-        if(option==1){
-            board.letPlayerPlayFirst(true);
-        }
-        int result = ui.play(board,ai);
+        int type = sc.nextInt();        
+        System.out.println("Play First?:Yes-1,No-2");
+        int chanceNo=sc.nextInt();
+        
+        ui.createPlayers(player,computer,type,chanceNo);        
+        int result = ui.play(board,ai,player,computer);
         board.printBoard();
         if (result == 0) {
             System.out.println("draw");
@@ -40,25 +42,25 @@ public class UI {
         sc.close();
     }
     
-    private int play(Board board,AI ai){
+    private int play(Board board,AI ai,Player player,Player computer){
         int turn=1;
-        if(!board.isPlayerFirst()){                                        // Comp plays odd turns           
+        if(computer.getChanceNo()==GameConstants.firstChance){                                        // Comp plays odd turns           
             for(;turn<=Board.TOTAL_MOVES;turn++){                           //9 turns
                 board.printBoard();
                 if (turn % 2 == 0)                          //PLayer's turn
                 {                                                                  
-                    board.setBoardIndex(this.getInput(board), board.getPlayer());                    
+                    board.setBoardIndex(this.getInput(board), player.getType());                    
                 }
                 else{                                       //main logic here,CPU's turn
                     if(turn==1){
-                        board.setBoardIndex(ai.getRandomCorner()-1,board.getComputer());
+                        board.setBoardIndex(ai.getRandomCorner()-1,computer.getType());
                     }
                     else{
-                        board.setBoardIndex(ai.findBestMove(board), board.getComputer());
-                        if(board.evaluateBoard()==Board.COMPUTER_WINS){
+                        board.setBoardIndex(ai.findBestMove(board,player,computer), computer.getType());
+                        if(board.evaluateBoard(player,computer)==Board.COMPUTER_WINS){
                                 return Board.COMPUTER_WINS;
                         }
-                        else if(board.evaluateBoard()==Board.PLAYER_WINS){
+                        else if(board.evaluateBoard(player,computer)==Board.PLAYER_WINS){
                                 return Board.PLAYER_WINS;
                         }
                     }
@@ -70,34 +72,34 @@ public class UI {
                 board.printBoard();
                 if (turn % 2 == 1)                          //PLayer's turn
                 {                   
-                    board.setBoardIndex(this.getInput(board), board.getPlayer()); 
+                    board.setBoardIndex(this.getInput(board), player.getType()); 
                 }
                 else{                                       //main logic here,CPU's turn
                     switch(turn){
                         case 2:{
                             if(!board.isBlank(4)){                        //center taken
-                                board.setBoardIndex(ai.getRandomCorner()-1, board.getComputer());//take any corner                        
+                                board.setBoardIndex(ai.getRandomCorner()-1, computer.getType());//take any corner                        
                             }
                             else if(!board.isBlank(0)||!board.isBlank(2)||!board.isBlank(6)||!board.isBlank(8)){    //corner taken
-                                board.setBoardIndex(4, board.getComputer());                               //take center
+                                board.setBoardIndex(4, computer.getType());                               //take center
                             }
                             else{                                   //edge taken
                                 if(!board.isBlank(1)||!board.isBlank(5))
-                                    board.setBoardIndex(2, board.getComputer());                                    
+                                    board.setBoardIndex(2, computer.getType());                                    
                                 else
-                                    board.setBoardIndex(6, board.getComputer());  
+                                    board.setBoardIndex(6, computer.getType());  
                             }
                             break;
                         }
                         case 4:
                         case 6:
                         case 8:{  
-                            board.setBoardIndex(ai.findBestMove(board), board.getComputer());                                
+                            board.setBoardIndex(ai.findBestMove(board,player,computer), computer.getType());                                
                             
-                            if(board.evaluateBoard()==Board.COMPUTER_WINS){
+                            if(board.evaluateBoard(player,computer)==Board.COMPUTER_WINS){
                                     return Board.COMPUTER_WINS;
                             }
-                            else if(board.evaluateBoard()==Board.PLAYER_WINS){
+                            else if(board.evaluateBoard(player,computer)==Board.PLAYER_WINS){
                                     return Board.PLAYER_WINS;
                             }
                         }    
@@ -118,5 +120,23 @@ public class UI {
         }while(!board.isBlank(nextMove-1));
         //sc.close();
         return nextMove-1;
+    }
+
+    private void createPlayers(Player player,Player computer,int type,int chanceNo) {
+        if(type==1){           
+            computer.setType(GameConstants.O);
+            player.setType(GameConstants.X);
+        }
+        else{            
+            computer.setType(GameConstants.X);
+            player.setType(GameConstants.O);
+        }
+        player.setChanceNo(chanceNo);
+        if(chanceNo==1){
+            computer.setChanceNo(GameConstants.secondChance);
+        }
+        else{
+            computer.setChanceNo(GameConstants.firstChance);
+        }
     }
 }
---
+++ src/tictactoe/ai/AI.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tictactoe.ai;
+
+import tictactoe.model.Board;
+import java.util.Random;
+import tictactoe.model.Player;
+
+/**
+ *
+ * @Raj Rahane
+ * 12 June 2018
+ */
+public class AI {
+    public int findBestMove(Board Board,Player player,Player computer){
+        int []board=Board.getBoard();
+        int bestValue=-1000;
+        int bestMove=-1;
+        int moveValue;
+        for(int i=0;i<Board.TOTAL_MOVES;i++){
+            if(Board.isBlank(i)){
+                board[i]=computer.getType();
+                moveValue=minimax(board,0,false,Board,player,computer);
+                board[i]=Board.BLANK;
+                
+                if(moveValue>bestValue){                    //***if multiple moves available, randomise
+                    bestValue=moveValue;
+                    bestMove=i;
+                }
+            }
+        }
+        return bestMove;
+    }
+    private int minimax(int[] board,int depth,boolean isMaximiserTurn,Board Board,Player player,Player computer){
+        int score=Board.evaluateBoard(player,computer);
+        if(score>0){                            //computer wins
+            return score-depth;
+        }
+        if(score<0){
+            return score+depth;
+        }        
+        
+        if(!Board.isMovesLeft()){
+            return 0;
+        }
+        int best;
+        if(isMaximiserTurn){
+            best=-1000;
+            for(int i=0;i<Board.TOTAL_MOVES;i++){
+                if(Board.isBlank(i)){
+                    board[i]=computer.getType();
+                    best=Math.max(best,minimax(board,depth+1,!isMaximiserTurn,Board,player,computer));
+                    board[i]=Board.BLANK;
+                }
+            }
+            return best-depth;
+        }
+        else{
+            best=1000;
+            int worstForUser=-1000;                                               //user may or may not play ideally,hence the variable to maximise win chances
+            for(int i=0;i<Board.TOTAL_MOVES;i++){
+                if(Board.isBlank(i)){
+                    board[i]=player.getType();
+                    best=Math.min(best,minimax(board,depth+1,!isMaximiserTurn,Board,player,computer));
+                    worstForUser=Math.max(worstForUser,minimax(board,depth+1,!isMaximiserTurn,Board,player,computer));
+                    board[i]=Board.BLANK;
+                }
+            }
+            if(best>0){
+                return worstForUser-depth;
+            }
+        }        
+        return best+depth;
+    }
+    public int getRandomCorner() {                             //selects random corner-1,3,7 or 9
+        int corners[]={1,3,7,9};                                // 1 based indexing
+        int rnd=new Random().nextInt(corners.length);
+        return corners[rnd];
+    }
+}
